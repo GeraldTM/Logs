cf00367ca9052cb304be55e2acf55c1ead9837b4
Gerald_TM <99144289+GeraldTM@users.noreply.github.com>
2022-03-16 22:10:18 -0400

    Save work for the day

--- Java/Logs.java
+++ Java/Logs.java
@@ -2,6 +2,7 @@ package Java;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,19 +10,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
+
 public class Logs {
     static String time = LocalTime.now().toString();
     static String date = LocalDate.now().toString();
     String ProgramLanguage = "java";
     static File programLog = new File("logs/"+ date + "/" +time +".log");
     static Path programLogPath = programLog.toPath();
-    FileWriter programlogwriter = new FileWriter(programLog);
-    public static void java(String[] args) {
-        try {
+    private void selfLogInit(String message) throws IOException {
             programLog.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } 
-
+            try (FileWriter programlogwriter = new FileWriter(programLog)) {
+            }
     }
 }
