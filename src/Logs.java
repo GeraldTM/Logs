ac1374226209ff227f844c539aa7a82a24e35e2f
Gerald_TM <99144289+GeraldTM@users.noreply.github.com>
2022-03-25 20:21:34 -0400

    long overdue save

--- src/Logs.java
+++ src/Logs.java
@@ -1,4 +1,4 @@
-package Java;
+package src;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -22,27 +22,39 @@ public class Logs {
     static String time = null;
     String ProgramLanguage = "java";
     Properties configLoad = new Properties();
+    File programLog;
+
+    public static void main(String[] args) {
+        //This method is for testing. run test.bat for full results.\
+        System.out.println("Test Succsesful");
+    }
+
     private String config(String property) throws IOException{
+
         FileInputStream configFile = new FileInputStream("Config.config");
         configLoad.load(configFile);
-        String reply = configLoad.getProperty(property).toString();
+        String reply = configLoad.getProperty(property).toString().toLowerCase();
         return reply;        
     };
     private void updateTime(){
+
         date = realDate;
         time = realTime;
     }
     private FileWriter selfLogInit() throws IOException {
+
             updateTime();
-            File programLog = new File("logs/"+ date + "/" +time +".log");
-            Path programLogPath = programLog.toPath();
+            if (config("Date_Folder")== "true"){
+                programLog = new File("logs/" + date + "/" + time +".log");
+            } else {
+                programLog = new File("logs/" +time + ".log");
+            }
             programLog.createNewFile();
             FileWriter programlogwriter = new FileWriter(programLog);
             return programlogwriter;
-
-            
     }
     private void selfLog( String message){
         
+
     }
 }
---
+++ src/test.bat
@@ -0,0 +1,3 @@
+cd "%~dp0"
+java Logs.java
+cmd /k
